a96890ea0c0b0cf3b75b8a3d9942508948a343ac
m1rageLA <miragela.code@gmail.com>
2024-05-03 17:31:11 +0200

    Add type declaration file

---
+++ src/components/Header/VueHeader.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        Hello world!
+    </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'VueHeader',
+});
+</script>
+<style lang="">
+
+</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,13 +1,17 @@
 <template lang="">
-    <div>
-        Hello world!
-    </div>
+    <VueHeader/>
 </template>
 <script lang="ts">
+
+
+import VueHeader from '@/components/Header/VueHeader.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'MainPage',
+    components: {
+        VueHeader
+    }
 });
 </script>
 <style lang="scss"></style>
